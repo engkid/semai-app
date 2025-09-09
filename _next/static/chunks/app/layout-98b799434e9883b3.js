84424537ea7decf8e172497ae23f1e26e6b09a32
Engkit Satia Riswara <engkidsr@gmail.com>
2025-09-09 16:40:00 +0700

    Deploy static site to GitHub Pages

---
+++ _next/static/chunks/app/layout-98b799434e9883b3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1452:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8380,23))},8380:()=>{}},e=>{e.O(0,[743,441,255,358],()=>e(e.s=1452)),_N_E=e.O()}]);
